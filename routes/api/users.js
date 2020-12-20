50d69f1ff76ebbbae3c3b8fb841dc5f434c9c82b
zhu <429299291@qq.com>
2020-12-20 16:00:14 +0800

    234

---
+++ routes/api/users.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/user')
+const bcrypt = require('bcrypt')
+const gravatar = require('gravatar');
+const user = require('../../models/user');
+router.get('/test',(req,res) =>{
+    res.json({
+        name:'login name'
+    })
+})
+router.post("/register",(req,res) => {
+    User.findOne({email:req.body.email})
+        .then(user =>{
+            if(user){
+                return res.status(400).json({email:'邮箱已被注册'})
+            }else{
+                const avatar = gravatar.url(req.body.email, {s: '200', r: 'pg', d: 'mm'});
+                const newUser = new User({
+                    name : req.body.name,
+                    email : req.body.email,
+                    avatar,
+                    password: req.body.password
+                })
+                // 密码加密模式
+                bcrypt.genSalt(10, function(err, salt) {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        // Store hash in your password DB.
+                        if(err) throw err
+                        newUser.password = hash;//加密过的密码
+                        newUser.save()
+                                .then(user => res.json(user))
+                                .catch(err => console.log(err))
+                    });
+                });
+
+            }
+        })
+})
+router.post("/login",(req,res) =>{
+    const email = req.body.email
+    const password = req.body.password
+    //查询数据库
+    User.findOne({email})
+        .then(user =>{
+            if(!user){
+                return res.status(404).json({email:'用户不存在'})
+            }
+            //密码匹配
+            bcrypt.compare(password, user.password, (err, result) =>{
+                // result == true
+                if(result == true){
+                    res.json({msg:'密码成功'})
+                }else{
+                    res.json({msg:'密码错误'})
+                }
+            });
+
+        })
+})
+module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const express = require('express')
 const mongoose = require('mongoose')
 const db = require("./config/keys.js").mongoURI
 const app = express()
+const bodyParser = require('body-parser')
+
+const users = require('./routes/api/users.js')
 
 mongoose.connect(db,{useNewUrlParser: true,useUnifiedTopology: true})
     .then(() =>{
-        console.log('mongoose成功')
+        console.log('mongoose数据库成功')
     })
     .catch(err =>{console.log(err)})
 app.get("/",(req,res) =>{
     res.send('hello world')
 })
+
+// bodyParser中间件
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use("/api/users",users)
 
 const port = process.env.PORT || 5000
 app.listen(port,() => {
